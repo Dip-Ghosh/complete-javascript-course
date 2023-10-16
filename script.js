d8970c252ae6b759d7ef05e8c4af2387deb96d05
Dip Ghosh <dip.ghosh@shohoz.com>
2023-10-16 23:09:27 +0600

    script seperate

---
+++ script.js
@@ -0,0 +1,13 @@
+let js = 'amazing';
+if (js === 'amazing') alert('Javascript is fun');
+
+let newJs = 'boring';
+if (newJs === 'amazing') alert('Javascript is fun');
+
+console.log('Dip');
+
+let firstName = "Dip";
+console.log(firstName);
+
+let PI = 3.1416
+console.log(PI);
