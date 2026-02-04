b11c2520c69cd8b8facd5a3b72c53fc3c07e9b82
Dip Ghosh <dipnsucse@gmail.com>
2026-02-04 13:48:26 +0600

    fundamentation done

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -1,13 +1,47 @@
-alert("Hello World!");
+// console.log("Hello World!");
+// let js = "amazing";
+// if (js === 'amazing') console.log('Javascript is fun');
+// js = "boring";
+// if (js === 'amazing') console.log('Javascript is fun');
+// 40+8+23+10
+// console.log(40+8+23+10);
 
-let js = "amazing";
+// math operator
+// const now = 2027;
+// const ageJonas = now - 1991;
+// const ageSearch = now - 2018;
+// console.log(ageJonas, ageSearch);
+// console.log(ageJonas * 2, ageSearch/10, 2**3);
 
-if (js === 'amazing') alert('Javascript is fun');
+// const firstName = 'John';
+// const lastName = 'Doe';
+// console.log(firstName + ' ' + lastName);
 
-js = "boring";
+//assignment operator
+// let x = 10+5;
+// x += 10;
+// x *= 4;
+// x /= 25;
+// x++;
+// x--;
+// ++x;
+// console.log(x);
 
-if (js === 'amazing') alert('Javascript is fun');
+//comparison operator
 
-40+8+23+10
-console.log(40+8+23+10);
+// console.log(10 > 12);
+// console.log(10 < 12);
+// console.log(10 >= 12);
+// console.log(10 <= 12);
+// console.log(2025 - 1991);
 
+// const now = 2037;
+// const age = now - 1994;
+// const ageSearch = now - 2018;
+//
+// console.log(ageSearch > age);
+// console.log(25 - 10 - 5);
+
+let x, y;
+x = y = 26 - 10 -5;
+console.log(x, y);
