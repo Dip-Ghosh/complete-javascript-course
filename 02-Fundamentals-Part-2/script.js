540eb8f3f2b6d2937903bd025a0391b8ee5d26f8
Dip Ghosh <dipnsucse@gmail.com>
2026-02-10 20:50:51 +0600

    strict mode introduce

---
+++ 02-Fundamentals-Part-2/script.js
@@ -0,0 +1,15 @@
+/**
+ * Strict mode for only specific function or blocks
+ */
+
+'use strict';
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log('I can drive');
+
+// const interface = 'Audio';
+// const private = 345;
+
