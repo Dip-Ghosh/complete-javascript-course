893c0e8faa47f546c169d3007583e1b672608f32
Dip Ghosh <dipnsucse@gmail.com>
2026-02-17 23:49:34 +0600

    challenge solve

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -295,3 +295,40 @@ const types = [];
 //     console.log(jonas[i], typeof jonas[i]);
 //     types.push(typeof jonas[i]);
 // }
+
+// for (let i = jonas.length -1; i >= 0 ; i--) {
+//     console.log(jonas[i], typeof jonas[i]);
+// }
+//
+// let i = 0
+// while(i< jonas.length) {
+//     console.log(jonas[i], typeof jonas[i]);
+//     i++;
+// }
+//
+// let dice = Math.trunc(Math.random() * 6) + 1;
+//
+// while (dice !== 6) {
+//     console.log(`Your dice is ${dice}`);
+//     dice = Math.trunc(Math.random() * 6) + 1;
+// }
+/**
+ * Challenge loop
+ */
+
+const calcTip = function (bill) {
+    return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+}
+
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+const tips = [];
+const totals = [];
+for (let i = 0; i < bills.length; i++) {
+    const tip = calcTip(bills[i]);
+    const bill = bills[i];
+    tips.push(tip);
+    totals.push(bill + tip);
+}
+
+console.log(tips);
+console.log(totals);
